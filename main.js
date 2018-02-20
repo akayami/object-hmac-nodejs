2d78fb8ecc1ed1746daf3c58560246f33bd7af70
Tomasz Rakowski <akayami@gmail.com>
2018-02-20 14:26:14 -0500

    Added tests.

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-var bencode = require('bencode-js');
-var crypto = require('crypto');
+const bencode = require('bencode');
+const crypto = require('crypto');
 
-module.exports = function(object, secret, hash) {
-	if(!hash) {
-		hash = 'sha512';
-	}
-	var string = bencode.encode(object);
+module.exports = function(object, secret, hash = 'sha512') {
+	const string = bencode.encode(object);
 	return crypto.createHmac(hash, secret).update(string).digest('hex');
-}
+};
